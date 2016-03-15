7e0ea84ba348f23e7d6cb39e44bd96a0a3ee8f14
blueghost56 <alex.ching5@gmail.com>
2016-03-15 16:48:09 +0800

    add locationmanager.java

---
+++ src/main/java/com/ways/os/location/LocationManager.java
@@ -0,0 +1,52 @@
+package com.ways.os.location;
+
+import android.content.Context;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+
+/**
+ * Created by wwzy on 16/3/15.
+ */
+public class LocationManager {
+    private LocationClient locationClient=null;
+
+    public LocationManager(Context context,BDLocationListener weatherListener){
+        locationClient=new LocationClient(context);
+        locationClient.registerLocationListener(weatherListener);
+        initLocation();
+    }
+
+    private void initLocation(){
+        LocationClientOption option=new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        option.setCoorType("bd09ll");
+        int span=1000;
+        option.setScanSpan(span);
+        option.setIsNeedAddress(true);
+        option.setOpenGps(true);
+        option.setLocationNotify(true);
+        option.setIsNeedLocationDescribe(true);
+        option.setIsNeedLocationPoiList(true);
+        option.setIgnoreKillProcess(false);
+        option.setEnableSimulateGps(false);
+        locationClient.setLocOption(option);
+
+    }
+
+
+
+    public void startLocation(){
+        if(!locationClient.isStarted())
+        locationClient.start();
+    }
+    public void stopLocation(){
+        if(locationClient.isStarted())
+        locationClient.stop();
+    }
+
+
+
+}
